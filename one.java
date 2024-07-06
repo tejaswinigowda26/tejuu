fe4d4e00075d462ae1f4e756fc84354785fc925e
bhoomika <bhoomika123@gmail.com>
2024-07-06 09:39:31 +0530

    change3

--- one.java
+++ one.java
@@ -3,7 +3,7 @@ public class one
 	public static void main(String [] args)
 	{
 		system.out.println("welcome world");
-	        system.out.println("mysore");
+	        system.out.println("mysore,banglore");
 	}
 
 }
